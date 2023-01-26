9c749e25a6dd2f950063cc109b159b4b5e282cb7
bruno-f-cruz <bfpcruz@gmail.com>
2023-01-26 01:01:49 +0000

    add table to class property dictionary

--- reflexgenerator/parser/__init__.py
+++ reflexgenerator/parser/__init__.py
@@ -2,11 +2,38 @@ from pathlib import Path
 
 import pandas as pd
 
+_col2mask = {
+    "Name": "name",
+    "MaskFamily": "mask_family",
+    "Mask": "mask",
+    "Format": "dtype",
+    "Description": "description",
+    "Converter": "converter",
+    "AutoInterface": "enable_generator"
+}
+
+_col2register = {
+    "Name": "name",
+    "Address": "address",
+    "Format": "dtype",
+    "PayloadFormat": "array_spec",
+    "Type": "is_event",
+    "Description": "description",
+    "MaskFamily": "mask_family",
+    "Converter": "converter",
+    "AutoInterface": "enable_generator"
+}
+
+_mask2col = dict([reversed(i) for i in _col2mask.items()])
+
+_register2col = dict([reversed(i) for i in _col2register.items()])
+
 
 class ElementParser():
     def __init__(
         self,
-        target: str | Path) -> pd.DataFrame:
+        target: str | Path
+        ) -> pd.DataFrame:
 
         if isinstance(target, Path):
             self.table = pd.read_csv(target)
