fa930baa5c9efc5e96fbdca04698640d207ebf77
glopesdev <glopesdev@users.noreply.github.com>
2025-01-28 21:21:14 +0000

    Add generator test infrastructure

---
+++ tests/Interface.Tests/TestTemplateGenerator.cs
@@ -0,0 +1,13 @@
+﻿using Mono.TextTemplating;
+
+namespace Interface.Tests;
+
+public class TestTemplateGenerator : TemplateGenerator
+{
+    protected override bool LoadIncludeText(string requestFileName, out string content, out string location)
+    {
+        content = TestHelper.GetManifestResourceText(requestFileName);
+        location = string.Empty;
+        return true;
+    }
+}
