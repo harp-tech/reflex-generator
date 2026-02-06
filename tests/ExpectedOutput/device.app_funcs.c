7336b85abb776b7200ed58a2bb0470b17638712c
glopesdev <g.lopes@neurogears.org>
2026-02-06 09:21:14 +0000

    Generate firmware app function stubs

---
+++ tests/ExpectedOutput/device.app_funcs.c
@@ -0,0 +1,79 @@
+#include "app_funcs.h"
+#include "app_ios_and_regs.h"
+#include "hwbp_core.h"
+
+
+/************************************************************************/
+/* Create pointers to functions                                         */
+/************************************************************************/
+extern AppRegs app_regs;
+
+void (*app_func_rd_pointer[])(void) = {
+	&app_read_REG_DIGITAL_INPUTS,
+	&app_read_REG_ANALOG_DATA,
+	&app_read_REG_COMPLEX_CONFIGURATION,
+	&app_read_REG_VERSION,
+	&app_read_REG_CUSTOM_PAYLOAD,
+	&app_read_REG_CUSTOM_RAW_PAYLOAD,
+	&app_read_REG_CUSTOM_MEMBER_CONVERTER,
+	&app_read_REG_BITMASK_SPLITTER,
+};
+
+bool (*app_func_wr_pointer[])(void*) = {
+	&app_write_REG_DIGITAL_INPUTS,
+	&app_write_REG_ANALOG_DATA,
+	&app_write_REG_COMPLEX_CONFIGURATION,
+	&app_write_REG_VERSION,
+	&app_write_REG_CUSTOM_PAYLOAD,
+	&app_write_REG_CUSTOM_RAW_PAYLOAD,
+	&app_write_REG_CUSTOM_MEMBER_CONVERTER,
+	&app_write_REG_BITMASK_SPLITTER,
+};
+
+/************************************************************************/
+/* REG_DIGITAL_INPUTS                                                   */
+/************************************************************************/
+void app_read_REG_DIGITAL_INPUTS(void) {}
+bool app_write_REG_DIGITAL_INPUTS(void *a) { return false; }
+
+/************************************************************************/
+/* REG_ANALOG_DATA                                                      */
+/************************************************************************/
+void app_read_REG_ANALOG_DATA(void) {}
+bool app_write_REG_ANALOG_DATA(void *a) { return false; }
+
+/************************************************************************/
+/* REG_COMPLEX_CONFIGURATION                                            */
+/************************************************************************/
+void app_read_REG_COMPLEX_CONFIGURATION(void) {}
+bool app_write_REG_COMPLEX_CONFIGURATION(void *a) { return true; }
+
+/************************************************************************/
+/* REG_VERSION                                                          */
+/************************************************************************/
+void app_read_REG_VERSION(void) {}
+bool app_write_REG_VERSION(void *a) { return false; }
+
+/************************************************************************/
+/* REG_CUSTOM_PAYLOAD                                                   */
+/************************************************************************/
+void app_read_REG_CUSTOM_PAYLOAD(void) {}
+bool app_write_REG_CUSTOM_PAYLOAD(void *a) { return false; }
+
+/************************************************************************/
+/* REG_CUSTOM_RAW_PAYLOAD                                               */
+/************************************************************************/
+void app_read_REG_CUSTOM_RAW_PAYLOAD(void) {}
+bool app_write_REG_CUSTOM_RAW_PAYLOAD(void *a) { return false; }
+
+/************************************************************************/
+/* REG_CUSTOM_MEMBER_CONVERTER                                          */
+/************************************************************************/
+void app_read_REG_CUSTOM_MEMBER_CONVERTER(void) {}
+bool app_write_REG_CUSTOM_MEMBER_CONVERTER(void *a) { return false; }
+
+/************************************************************************/
+/* REG_BITMASK_SPLITTER                                                 */
+/************************************************************************/
+void app_read_REG_BITMASK_SPLITTER(void) {}
+bool app_write_REG_BITMASK_SPLITTER(void *a) { return true; }
--- tests/FirmwareGeneratorTests.cs
+++ tests/FirmwareGeneratorTests.cs
@@ -9,6 +9,7 @@ public sealed class FirmwareGeneratorTests
     TemplateGenerator generator;
     CompiledTemplate appTemplate;
     CompiledTemplate appFuncsTemplate;
+    CompiledTemplate appFuncsImplTemplate;
     CompiledTemplate appRegsTemplate;
     CompiledTemplate appRegsImplTemplate;
     DirectoryInfo outputDirectory;
@@ -19,12 +20,16 @@ public sealed class FirmwareGeneratorTests
         generator = new TestTemplateGenerator();
         var appTemplateContents = TestHelper.GetManifestResourceText("App.tt");
         var appFuncsTemplateContents = TestHelper.GetManifestResourceText("AppFuncs.tt");
+        var appFuncsImplTemplateContents = TestHelper.GetManifestResourceText("AppFuncsImpl.tt");
         var appRegsTemplateContents = TestHelper.GetManifestResourceText("AppRegs.tt");
         var appRegsImplTemplateContents = TestHelper.GetManifestResourceText("AppRegsImpl.tt");
         appTemplate = await generator.CompileTemplateAsync(appTemplateContents);
         TestHelper.AssertNoGeneratorErrors(generator);
 
         appFuncsTemplate = await generator.CompileTemplateAsync(appFuncsTemplateContents);
+        TestHelper.AssertNoGeneratorErrors(generator);
+
+        appFuncsImplTemplate = await generator.CompileTemplateAsync(appFuncsImplTemplateContents);
         TestHelper.AssertNoGeneratorErrors(generator);
 
         appRegsTemplate = await generator.CompileTemplateAsync(appRegsTemplateContents);
@@ -57,6 +62,9 @@ public sealed class FirmwareGeneratorTests
         var appFuncsCode = ProcessTemplate(appFuncsTemplate, metadataFileName);
         TestHelper.AssertNoGeneratorErrors(generator);
 
+        var appFuncsImplCode = ProcessTemplate(appFuncsImplTemplate, metadataFileName);
+        TestHelper.AssertNoGeneratorErrors(generator);
+
         var iosMetadataFileName = Path.ChangeExtension(metadataFileName, ".ios.yml");
         var appRegsCode = ProcessTemplate(appRegsTemplate, metadataFileName, iosMetadataFileName);
         TestHelper.AssertNoGeneratorErrors(generator);
@@ -67,12 +75,14 @@ public sealed class FirmwareGeneratorTests
         var outputFileName = Path.GetFileNameWithoutExtension(metadataFileName);
         var appOutputFileName = $"{outputFileName}.app.h";
         var appFuncsOutputFileName = $"{outputFileName}.app_funcs.h";
+        var appFuncsImplOutputFileName = $"{outputFileName}.app_funcs.c";
         var appRegsOutputFileName = $"{outputFileName}.app_ios_and_regs.h";
         var appRegsImplOutputFileName = $"{outputFileName}.app_ios_and_regs.c";
         try
         {
             TestHelper.AssertExpectedOutput(appCode, appOutputFileName);
             TestHelper.AssertExpectedOutput(appFuncsCode, appFuncsOutputFileName);
+            TestHelper.AssertExpectedOutput(appFuncsImplCode, appFuncsImplOutputFileName);
             TestHelper.AssertExpectedOutput(appRegsCode, appRegsOutputFileName);
             TestHelper.AssertExpectedOutput(appRegsImplCode, appRegsImplOutputFileName);
         }
@@ -81,6 +91,7 @@ public sealed class FirmwareGeneratorTests
             outputDirectory.Create();
             File.WriteAllText(Path.Combine(outputDirectory.FullName, appOutputFileName), appCode);
             File.WriteAllText(Path.Combine(outputDirectory.FullName, appFuncsOutputFileName), appFuncsCode);
+            File.WriteAllText(Path.Combine(outputDirectory.FullName, appFuncsImplOutputFileName), appFuncsImplCode);
             File.WriteAllText(Path.Combine(outputDirectory.FullName, appRegsOutputFileName), appRegsCode);
             File.WriteAllText(Path.Combine(outputDirectory.FullName, appRegsImplOutputFileName), appRegsImplCode);
             throw;
