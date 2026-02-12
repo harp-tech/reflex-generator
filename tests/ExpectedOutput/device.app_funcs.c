1dd6013a25934e619db329c73bcce8d9526abede
glopesdev <g.lopes@neurogears.org>
2026-02-12 21:21:38 +0000

    Generate register template functions
    Provides stubs for accessing both single value and array registers.
    Read-only registers have empty write function stubs.

--- tests/ExpectedOutput/device.app_funcs.c
+++ tests/ExpectedOutput/device.app_funcs.c
@@ -41,71 +41,167 @@ bool (*app_func_wr_pointer[])(void*) = {
 /************************************************************************/
 /* REG_DIGITAL_INPUTS                                                   */
 /************************************************************************/
-void app_read_REG_DIGITAL_INPUTS(void) {}
+void app_read_REG_DIGITAL_INPUTS(void)
+{
+	//app_regs.REG_DIGITAL_INPUTS = 0;
+
+}
+
 bool app_write_REG_DIGITAL_INPUTS(void *a) { return false; }
 
 /************************************************************************/
 /* REG_ANALOG_DATA                                                      */
 /************************************************************************/
-void app_read_REG_ANALOG_DATA(void) {}
+// This register is an array with 6 positions
+void app_read_REG_ANALOG_DATA(void)
+{
+	//app_regs.REG_ANALOG_DATA[0] = 0;
+
+}
+
 bool app_write_REG_ANALOG_DATA(void *a) { return false; }
 
 /************************************************************************/
 /* REG_COMPLEX_CONFIGURATION                                            */
 /************************************************************************/
-void app_read_REG_COMPLEX_CONFIGURATION(void) {}
-bool app_write_REG_COMPLEX_CONFIGURATION(void *a) { return true; }
+// This register is an array with 17 positions
+void app_read_REG_COMPLEX_CONFIGURATION(void)
+{
+	//app_regs.REG_COMPLEX_CONFIGURATION[0] = 0;
+
+}
+
+bool app_write_REG_COMPLEX_CONFIGURATION(void *a)
+{
+	uint8_t *reg = ((uint8_t*)a);
+
+	app_regs.REG_COMPLEX_CONFIGURATION[0] = reg[0];
+    return true;
+}
 
 /************************************************************************/
 /* REG_VERSION                                                          */
 /************************************************************************/
-void app_read_REG_VERSION(void) {}
+// This register is an array with 32 positions
+void app_read_REG_VERSION(void)
+{
+	//app_regs.REG_VERSION[0] = 0;
+
+}
+
 bool app_write_REG_VERSION(void *a) { return false; }
 
 /************************************************************************/
 /* REG_CUSTOM_PAYLOAD                                                   */
 /************************************************************************/
-void app_read_REG_CUSTOM_PAYLOAD(void) {}
+// This register is an array with 3 positions
+void app_read_REG_CUSTOM_PAYLOAD(void)
+{
+	//app_regs.REG_CUSTOM_PAYLOAD[0] = 0;
+
+}
+
 bool app_write_REG_CUSTOM_PAYLOAD(void *a) { return false; }
 
 /************************************************************************/
 /* REG_CUSTOM_RAW_PAYLOAD                                               */
 /************************************************************************/
-void app_read_REG_CUSTOM_RAW_PAYLOAD(void) {}
+// This register is an array with 3 positions
+void app_read_REG_CUSTOM_RAW_PAYLOAD(void)
+{
+	//app_regs.REG_CUSTOM_RAW_PAYLOAD[0] = 0;
+
+}
+
 bool app_write_REG_CUSTOM_RAW_PAYLOAD(void *a) { return false; }
 
 /************************************************************************/
 /* REG_CUSTOM_MEMBER_CONVERTER                                          */
 /************************************************************************/
-void app_read_REG_CUSTOM_MEMBER_CONVERTER(void) {}
+// This register is an array with 3 positions
+void app_read_REG_CUSTOM_MEMBER_CONVERTER(void)
+{
+	//app_regs.REG_CUSTOM_MEMBER_CONVERTER[0] = 0;
+
+}
+
 bool app_write_REG_CUSTOM_MEMBER_CONVERTER(void *a) { return false; }
 
 /************************************************************************/
 /* REG_BITMASK_SPLITTER                                                 */
 /************************************************************************/
-void app_read_REG_BITMASK_SPLITTER(void) {}
-bool app_write_REG_BITMASK_SPLITTER(void *a) { return true; }
+void app_read_REG_BITMASK_SPLITTER(void)
+{
+	//app_regs.REG_BITMASK_SPLITTER = 0;
+
+}
+
+bool app_write_REG_BITMASK_SPLITTER(void *a)
+{
+	uint8_t reg = *((uint8_t*)a);
+
+	app_regs.REG_BITMASK_SPLITTER = reg;
+    return true;
+}
 
 /************************************************************************/
 /* REG_COUNTER0                                                         */
 /************************************************************************/
-void app_read_REG_COUNTER0(void) {}
+void app_read_REG_COUNTER0(void)
+{
+	//app_regs.REG_COUNTER0 = 0;
+
+}
+
 bool app_write_REG_COUNTER0(void *a) { return false; }
 
 /************************************************************************/
 /* REG_PORT_DIO_SET                                                     */
 /************************************************************************/
-void app_read_REG_PORT_DIO_SET(void) {}
-bool app_write_REG_PORT_DIO_SET(void *a) { return true; }
+void app_read_REG_PORT_DIO_SET(void)
+{
+	//app_regs.REG_PORT_DIO_SET = 0;
+
+}
+
+bool app_write_REG_PORT_DIO_SET(void *a)
+{
+	uint8_t reg = *((uint8_t*)a);
+
+	app_regs.REG_PORT_DIO_SET = reg;
+    return true;
+}
 
 /************************************************************************/
 /* REG_PULSE_DO_PORT0                                                   */
 /************************************************************************/
-void app_read_REG_PULSE_DO_PORT0(void) {}
-bool app_write_REG_PULSE_DO_PORT0(void *a) { return true; }
+void app_read_REG_PULSE_DO_PORT0(void)
+{
+	//app_regs.REG_PULSE_DO_PORT0 = 0;
+
+}
+
+bool app_write_REG_PULSE_DO_PORT0(void *a)
+{
+	uint16_t reg = *((uint16_t*)a);
+
+	app_regs.REG_PULSE_DO_PORT0 = reg;
+    return true;
+}
 
 /************************************************************************/
 /* REG_PULSE_DO0                                                        */
 /************************************************************************/
-void app_read_REG_PULSE_DO0(void) {}
-bool app_write_REG_PULSE_DO0(void *a) { return true; }
+void app_read_REG_PULSE_DO0(void)
+{
+	//app_regs.REG_PULSE_DO0 = 0;
+
+}
+
+bool app_write_REG_PULSE_DO0(void *a)
+{
+	uint16_t reg = *((uint16_t*)a);
+
+	app_regs.REG_PULSE_DO0 = reg;
+    return true;
+}
